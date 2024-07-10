330b045e11b22265ba32f0ee7b96a66be8b3a4e6
alfonso <alfonsoa@zillowgroup.com>
2024-07-10 17:53:07 -0600

    adding elpa folder

---
+++ .emacs.d/elpa/corfu-candidate-overlay-20240322.1814/corfu-candidate-overlay-pkg.el
@@ -0,0 +1,13 @@
+(define-package "corfu-candidate-overlay" "20240322.1814" "Show first candidate in an overlay while typing"
+  '((emacs "28.1")
+    (corfu "0.36"))
+  :commit "f730de2c150720ee70d4d7be4b8bb533c7dfc97e" :authors
+  '(("Adam Kruszewski" . "adam@kruszewski.name"))
+  :maintainers
+  '(("Adam Kruszewski" . "adam@kruszewski.name"))
+  :maintainer
+  '("Adam Kruszewski" . "adam@kruszewski.name")
+  :url "https://code.bsdgeek.org/adam/corfu-candidate-overlay/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
